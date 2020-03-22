809635927967e812d0bd1ccbb3a498361acd0516
guo-yaohua <guo.yaohua@qq.com>
2020-03-22 12:15:22 +0800

    ok

--- before/ex/Ex50.java
+++ before/ex/Ex50.java
@@ -26,7 +26,7 @@ public class Ex50 {
 
 		// 输入
 		Scanner sc = new Scanner(System.in);
-		int i;
+		int i, j;
 		for (i = 0; i < 5; i++) {
 			System.out.printf("现在输入第 %d 个学生的信息\n学号：", i+1);
 			studentId[i] = sc.nextInt();
@@ -35,8 +35,8 @@ public class Ex50 {
 			System.out.print("姓名：");
 			studentName[i] = sc.nextLine();
 
-			for (int j = 0; j < 3; j++) {
-				System.out.printf("第 %d 门课的成绩：", j + 1);
+			for (j = 0; j < 3; j++) {
+				System.out.printf("课 %d 的成绩：", j + 1);
 				studentCourse[i][j] = sc.nextInt();
 			}
 		}
@@ -44,19 +44,30 @@ public class Ex50 {
 		// 计算
 		for (i = 0; i < 3; i++) {
 			averageScore[i] = 0;
-			for (int j = 0; j < 5; j++) {
+			for (j = 0; j < 5; j++) {
 				averageScore[i] += studentCourse[j][i];
 			}
 			averageScore[i] /= 5;
-			System.out.printf("\n第 %d 门课的平均成绩为：%.2f\n", i + 1, averageScore[i]);
+			System.out.printf("\n课 %d 的平均成绩为：%.2f\n", i + 1, averageScore[i]);
 		}
 
 		// 写入文件
 		File file = new File("stud.txt");
 		file.createNewFile();
 		FileWriter out = new FileWriter(file);
-		String str = "qaq";
-		out.write(str);
+
+		// String 是最大的类型，由小到大可以自动转换
+		out.write("学号\t姓名\t课1\t课2\t课3\n");
+		for (i = 0; i < 5; i++) {
+			out.write(studentId[i] + "\t" + studentName[i] + "\t");
+			for (j = 0; j < 3; j++) {
+				out.write(studentCourse[i][j] + "\t");
+			}
+			out.write("\n");
+		}
+		for (i = 0; i < 3; i++) {
+			out.write("课 " + (i + 1) + " 的平均成绩：" + averageScore[i] + "\n");
+		}
 		out.flush();
 		out.close();
 	}
