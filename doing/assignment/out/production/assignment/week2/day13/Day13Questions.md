525767f2089ba692d25548a7cd72281f1555835a
guo-yaohua <guo.yaohua@qq.com>
2020-04-20 22:09:50 +0800

    day13

---
+++ doing/assignment/out/production/assignment/week2/day13/Day13Questions.md
@@ -0,0 +1,30 @@
+# Day 13 JavaObject03
+
+1. 为某研究所编写一个通用程序，用来计算每一种交通工具运行 1000 公里所需的时间。已知每种交通工具的参数都是 3 个整数常量 A、B、C 的表达式。  
+  现有两种工具：Car 和 Plane，  
+  其中 Car 的速度运算公式为：A * B / C，  
+  Plane 的速度运算公式为  ：A + B + C。  
+  要求在未来如果增加第 3 种交通工具（如 Ship）的时候，可以做到复用已有代码。  
+  Ship 的速度运算公式为  ：A + B / C。  
+  要求自己设计该系统，并测试。
+  
+2. 完成课堂上没有讲完的案例：  
+  教练和运动员案例  
+  乒乓球运动员和篮球运动员。  
+  乒乓球教练和篮球教练。  
+  为了出国交流，跟乒乓球相关的人员都需要学习英语。  
+  请用所学知识：  
+  分析，这个案例中有哪些抽象类，哪些接口，哪些具体类  
+    ```
+    乒乓球用运动员 属性  name age salary learnEnglish()
+                行为 eat() sleep() train() match()
+    
+    篮球运动员   属性  name age salary
+                行为 eat() sleep() train() match()
+    
+    乒乓球教练:  nama age salary bonus
+                eat()，sleep(), teach()，learnEnglish()
+    
+    篮球教练：   nama age salary bonus
+                eat()，sleep(), teach()
+    ```
---
+++ doing/assignment/src/week2/day13/Solution2.java
@@ -0,0 +1,128 @@
+package week2.day13;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        TableTennisAthlete tableTennisAthlete = new TableTennisAthlete("张三", 18, 2000);
+        tableTennisAthlete.eat();
+        tableTennisAthlete.train();
+        tableTennisAthlete.learnEnglish();
+    }
+}
+
+// 人类
+abstract class Human {
+    String name;
+    int age;
+    int salary;
+
+    Human(String name, int age, int salary){
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public void eat() {
+        System.out.println(this.name + "在吃饭！");
+    }
+    public void sleep() {
+        System.out.println(this.name + "在睡觉！");
+    }
+}
+
+// 教练员
+abstract class Coach extends Human {
+    int bonus;
+
+    Coach(String name, int age, int salary, int bonus) {
+        super(name, age, salary);
+        this.bonus = bonus;
+    }
+
+    public abstract void teach();
+}
+
+// 运动员
+
+abstract class Athlete extends Human {
+    Athlete(String name, int age, int salary) {
+        super(name, age, salary);
+    }
+
+    public abstract void train();
+    public abstract void match();
+}
+
+// 接口：学习英语
+interface SpecialSkill {
+    public void learnEnglish();
+}
+
+// 乒乓球运动员
+class TableTennisAthlete extends Athlete implements SpecialSkill {
+    TableTennisAthlete(String name, int age, int salary) {
+        super(name, age, salary);
+    }
+
+    @Override
+    public void train() {
+        System.out.println(this.name + "在乒乓球训练");
+    }
+
+    @Override
+    public void match() {
+        System.out.println(this.name + "在乒乓球比赛");
+    }
+
+    @Override
+    public void learnEnglish() {
+        System.out.println(this.name + "在学习英语");
+    }
+}
+
+// 乒乓球教练
+class TableTennisCoach extends Coach implements SpecialSkill {
+
+    TableTennisCoach(String name, int age, int salary, int bonus) {
+        super(name, age, salary, bonus);
+    }
+
+    @Override
+    public void teach() {
+        System.out.println(this.name + "在乒乓球教学");
+    }
+
+    @Override
+    public void learnEnglish() {
+        System.out.println(this.name + "在学习英语");
+    }
+}
+
+// 篮球运动员
+class BasketballAthlete extends Athlete {
+
+    BasketballAthlete(String name, int age, int salary) {
+        super(name, age, salary);
+    }
+
+    @Override
+    public void train() {
+        System.out.println(this.name + "在蓝球训练");
+    }
+
+    @Override
+    public void match() {
+        System.out.println(this.name + "在蓝球比赛");
+    }
+}
+
+class BasketballCoach extends Coach {
+
+    BasketballCoach(String name, int age, int salary, int bonus) {
+        super(name, age, salary, bonus);
+    }
+
+    @Override
+    public void teach() {
+        System.out.println(this.name + "在篮球教学");
+    }
+}
