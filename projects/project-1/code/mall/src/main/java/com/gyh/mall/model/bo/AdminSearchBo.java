3e55cf2f27c78ab90b074c66b958598f10931040
gyh <guo.yaohua@qq.com>
2020-07-04 15:02:02 +0800

    多条件查询

---
+++ projects/project-1/code/mall/src/main/java/com/gyh/mall/model/bo/AdminSearchBo.java
@@ -0,0 +1,24 @@
+package com.gyh.mall.model.bo;
+
+public class AdminSearchBo {
+
+    private String email;
+
+    private String nickname;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
